43f47167047f2b697df08c84af2f3a36403970a0
dymondgeezer <andy_dymond@hotmail.co.uk>
2026-05-28 23:04:50 +0100

    Update token.js

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -1,1 +1,23 @@
-
+export const handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not allowed' };
+  }
+  try {
+    const response = await fetch('https://identity.xero.com/connect/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: event.body,
+    });
+    const data = await response.text();
+    return {
+      statusCode: response.status,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: data,
+    };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+};
